cd98ed05071f3e82335146c8e275ddf67b27a1c4
Siyuan <siyuan.xiang@hyperscience.com>
2025-07-21 14:28:36 -0400

    update version to 0.42.1+hs0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ GitHub: https://github.com/fxsjy/jieba
 """
 
 setup(name='jieba',
-      version='0.42.1',
+      version='0.42.1+hs0',
       description='Chinese Words Segmentation Utilities',
       long_description=LONGDOC,
       author='Sun, Junyi',
