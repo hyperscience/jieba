4da9c2b98f920cf2fa0bcb01ac39ee3bb35ae0b6
Dimitar Kovachev <mitko@hyperscience.com>
2025-07-25 15:51:31 +0300

    wire: autotag

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ GitHub: https://github.com/fxsjy/jieba
 """
 
 setup(name='jieba',
-      version='0.42.1+hs0',
+      version='0.42.1.dev0',
       description='Chinese Words Segmentation Utilities',
       long_description=LONGDOC,
       author='Sun, Junyi',
